731e9dd732f740e5fc41554c9923c0353c2957da
pytlab <shaozhengjiang@gmail.com>
2018-01-23 15:11:02 +0800

    Add gradients computation function.

--- simpleflow/operations.py
+++ simpleflow/operations.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 ''' Operation classes in computational graph.
 '''
+from queue import Queue
+
 import numpy as np
 
 class Operation(object):
@@ -311,4 +313,63 @@ class Placeholder(object):
 
         # Add to the currently active default graph.
         self.graph.placeholders.append(self)
+
+
+# ------------------------------------------------------------------------------
+# Function for gradients computation.
+# ------------------------------------------------------------------------------
+
+def compute_gradients(target_op):
+    ''' Backpropagation implementation computing gradient of target operation wrt
+        all the other connected nodes.
+
+    :param target_op: The target operation whose gradient wrt other nodes would
+                      be computed.
+    :type target_op: Any operation type.
+
+    :return grad_table: A table containing node objects and gradients.
+    :type grad_table: dict.
+    '''
+    # A dict containing a mapping between node and gradient value of target_op wrt the node's output.
+    # NOTE: It is the gradient wrt the node's OUTPUT NOT input.
+    grad_table = {}
+
+    # The gradient wrt target_op itself is 1.
+    grad_table[target_op] = np.ones_like(target_op.output_value)
+
+    # Perform a breadth-first search staring from the target_op in graph.
+    # Queue for node traverasl.
+    queue = Queue()
+    queue.put(target_op)
+
+    # Set for visited nodes.
+    visited = set()
+    visited.add(target_op)
+
+    while not queue.empty():
+        node = queue.get()
+
+        # Compute gradient wrt the node's output.
+        if node != target_op:
+            grads_wrt_node_output = []
+            for output_node in node.output_nodes:
+                # Retrieve the gradient wrt output_node's OUTPUT.
+                grad_wrt_output_node_output = grad_table[output_node]
+                # Compute the gradient wrt current node's output.
+                grad_wrt_node_output = output_node.compute_gradient(grad_wrt_output_node_output)
+                input_node_index = output_node.input_nodes.index(node)
+                grads_wrt_node_output.append(grad_wrt_node_output[input_node_index])
+
+            # Sum all gradients wrt node's output.
+            tot_grad_wrt_node_output = sum(grads_wrt_node_output)
+            grad_table[node] = tot_grad_wrt_node_output
+
+        # Put adjecent nodes to queue.
+        if hasattr(node, 'input_nodes'):
+            for input_node in node.input_nodes:
+                if input_node not in visited:
+                    visited.add(input_node)
+                    queue.put(input_node)
+
+    return grad_table
 
